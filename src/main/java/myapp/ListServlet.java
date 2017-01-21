e53efd152faefa58c75ff3d6ef8a6d0c9ae460ef
DanDirector <Dandirector@mail.ru>
2017-01-21 11:17:14 +0200

    Update ListServlet.java

--- src/main/java/myapp/ListServlet.java
+++ src/main/java/myapp/ListServlet.java
@@ -1,1 +1,15 @@
+package myapp;
 
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ListServlet extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+    resp.setContentType("text/plain");
+    resp.getWriter().println("{ \"name\": \"Danil\" }");
+  }
+}
